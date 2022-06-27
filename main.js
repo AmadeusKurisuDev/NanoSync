6f74a3ae6fee0b01bcb2d4bd577039e790b859bc
Michele_Felici <michelefelice58@gmail.com>
2022-06-27 14:04:44 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+//Creato da Michele Felici 
+//Tutti i diritti riservati. 
+ 
+// Modules 
+const {app, BrowserWindow, Menu} = require('electron') 
+const colors = require('colors') 
+ 
+console.log(colors.rainbow('Hello World!')) 
+let mainWindow 
+ 
+// Create a new BrowserWindow when `app` is ready 
+function createWindow () { 
+ 
+ mainWindow = new BrowserWindow({ 
+   width: 1000, height: 800, 
+   webPreferences: { 
+     // --- !! IMPORTANT !! --- 
+     // Disable 'contextIsolation' to allow 'nodeIntegration' 
+     // 'contextIsolation' defaults to "true" as from Electron v12 
+     contextIsolation: false, 
+     nodeIntegration: true 
+   } 
+ }) 
+  
+ // remove the menu on the app, but not the main buttons 
+ mainWindow.setMenuBarVisibility(false), 
+ // Load index.html into the new BrowserWindow 
+  mainWindow.loadFile('index.html') 
+ 
+ // Open DevTools - Remove for PRODUCTION! 
+ // mainWindow.webContents.openDevTools(); 
+ 
+ // mainWindow.once('ready-to-show', mainWindow.show) 
+ 
+ // Listen for window being closed 
+ mainWindow.on('closed',  () => { 
+   mainWindow = null 
+ }) 
+ 
+} 
+ 
+// Quit when all windows are closed - (Not macOS - Darwin) 
+app.on('window-all-closed', () => { 
+ if (process.platform !== 'darwin') app.quit() 
+}) 
+ 
+// Electron `app` is ready 
+app.on('ready', createWindow) 
+  
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow 
+app.on('activate', () => { 
+ if (mainWindow === null) createWindow() 
+}) 
